93bfccb5c24bad959a963f589d18bbadca190a90
Rami1125 <rami.msarwa1@gmail.com>
2026-07-07 07:29:20 +0300

    Update orders-board.tsx

--- components/orders-board.tsx
+++ components/orders-board.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import { collection, onSnapshot, doc, updateDoc, query, orderBy } from 'firebase/firestore';
 import { db } from '../lib/firebase'; // ודא שהנתיב תואם לפרויקט שלך
