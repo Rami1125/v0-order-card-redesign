91959997f3e9c292d58925315e267ac46e369c9a
Rami1125 <rami.msarwa1@gmail.com>
2026-07-09 04:38:10 +0300

    Update firebase-admin.ts

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,9 +1,12 @@
 import * as admin from 'firebase-admin';
 
+// הוספנו את השם המלא של המשתנה project_id כפי שהוא מופיע ב-JSON שלך
+const projectId = process.env.FIREBASE_PROJECT_ID || "thematic-ruler-501808-m1";
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID, // וודא שזה קיים ב-Vercel
+      projectId: projectId,
       clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
       privateKey: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
